b334a824221dde69d4b3413fa1b5802693fe5b98
Marco Dal Molin <marco.dalmolin.1991@gmail.com>
2026-07-02 19:18:45 +0200

    Added comment

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  /* Important - the url https://gliattomatti.ch/Saalvermietung is used behind many printed QR codes - do not kill it */
   async redirects() {
     return [
       {
