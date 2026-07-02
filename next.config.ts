c013aac74d4172f3322cd2ca4bf1457ffdfd4b8b
Marco Dal Molin <marco.dalmolin.1991@gmail.com>
2026-07-02 18:49:59 +0200

    Added redirect to booking system

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/Saalvermietung',
+        destination: 'https://tally.so/r/LZaPOz',
+        permanent: false,
+      },
+      {
+        source: '/saalvermietung',
+        destination: 'https://tally.so/r/LZaPOz',
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
